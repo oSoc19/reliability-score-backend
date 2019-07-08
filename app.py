23202054c5a420a2d39c50004a8887ddf2d22712
Dylan Van Assche <dylan.van.assche@protonmail.com>
2019-07-08 18:11:45 +0200

    [WIP] Querying and grouping stops

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ from tornado.web import Application, RequestHandler
 from tornado.ioloop import IOLoop
 from root import RootHandler
 from connections import ConnectionsHandler
+from convert import ConvertHandler
 
 if __name__ == "__main__":
     urls = [("/", RootHandler),
-    ("/connections", ConnectionsHandler)]
+    ("/connections", ConnectionsHandler),
+    ("/convert", ConvertHandler)]
 
     app = Application(urls, debug=True)
     app.listen(3000)
+
     IOLoop.instance().start()
