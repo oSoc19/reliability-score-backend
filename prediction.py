68362e7b21ecc1b7a56ece49192bc6126260704e
Sam van der Kris <samvdkris@gmail.com>
2019-07-08 11:45:40 +0200

    Add dummy API
    This loads an example iRail API response from a locally stored JSON file, adds randomly generated reliability data and then returns it.

--- prediction.py
+++ prediction.py
@@ -1,14 +1,37 @@
 #!/usr/bin/env python
 from tornado.web import RequestHandler, MissingArgumentError
 
+import json
+import re
+from random import randint
+
+
+def predictDelays(connection):
+	connection['delayPrediction'] = randint(-100, 200)
+	connection['delayChance'] = randint(0, 100)
+	return connection
+
+
 class PredictionHandler(RequestHandler):
-    def get(self):
-        station = self.get_query_argument("station", default=None)
-        connection = self.get_query_argument("connection", default=None)
-        if station:
-            print("station")
-        elif connection:
-            print("connection")
-        else:
-            raise MissingArgumentError("Please provide a station or connection URI")
-        self.write({"message": "hello world"})
+	def get(self):
+		connection = self.get_query_argument("connection", default=None)
+		if not connection:
+			raise MissingArgumentError("Please provide a station or connection URI")
+
+		response = {}
+
+		# Get iRail data, will use actual API in future
+		with open('irail-example.json') as file:
+			irail = json.load(file)
+
+		# Add reliability score data
+		# NOTE: delay info is an integer, rest of iRail API uses strings
+		response = irail
+		for connection in response['connection']:
+			connection['arrival'] = predictDelays(connection['arrival'])
+			connection['departure'] = predictDelays(connection['departure'])
+			for via in connection['vias']['via']:
+				via['arrival'] = predictDelays(via['arrival'])
+				via['departure'] = predictDelays(via['arrival'])
+
+		self.write(irail)
