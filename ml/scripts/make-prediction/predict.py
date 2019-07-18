4a81ba88d22c7a5523769a63e1754273058cf5d6
Sam van der Kris <samvdkris@gmail.com>
2019-07-18 11:09:26 +0200

    Change ML data path and autoformatting

--- ml/scripts/make-prediction/predict.py
+++ ml/scripts/make-prediction/predict.py
@@ -16,12 +16,9 @@ cat.load_model('test_model.cbm')
 after = time.time()
 print(f'Loading model: {after-before} sec.')
 
-
-stations_df = pd.read_csv('../data/stations.csv')
-station_uris = json.load(open('../data/station_uris_irail.json', 'r'))
+stations_df = pd.read_csv('../../data/stations.csv')
+station_uris = json.load(open('../../data/station_uris_irail.json', 'r'))
 encoders = pickle.load(open('encoders.pickle', 'rb'))
-
-
 
 
 def station_to_uri(station):
@@ -32,7 +29,7 @@ def station_to_uri(station):
 
 
 def get_seconds_since_midnight(datetime):
-	midnight =  datetime.replace(hour=0, minute=0, second=0, microsecond=0)
+	midnight = datetime.replace(hour=0, minute=0, second=0, microsecond=0)
 	return (datetime - midnight).total_seconds()
 
 
@@ -57,12 +54,12 @@ def encode(feature, value):
 	return encoders[feature].transform([value])
 
 
-
-
 def get_features(vehicle_id, time_str):
 	parsed_time = pd.to_datetime(time_str, format='%d-%m-%Y %H:%M')
 	parsed_time_str = parsed_time.strftime('%d%m%y')
-	VEHICLE_URL = 'http://api.irail.be/vehicle/?id={}&date={}&format=json&lang=nl'.format(vehicle_id, parsed_time_str)
+	VEHICLE_URL = 'http://api.irail.be/vehicle/?id={}&date={}&format=json&lang=nl'.format(
+	    vehicle_id, parsed_time_str
+	)
 
 	line_id = vehicle_id.split('.')[-1]
 	pattern = re.compile("^([A-Z]+)([0-9]+)$")
@@ -71,11 +68,16 @@ def get_features(vehicle_id, time_str):
 
 	resp = requests.get(VEHICLE_URL)
 	resp = json.loads(resp.content)
-	uri_dep = resp['stops']['stop'][0]['stationinfo']['@id']  # Name of the departure station
-	stop_dep = int(stations_df[stations_df['URI'] == uri_dep].iloc[0, :]['avg_stop_times'])
+	uri_dep = resp['stops']['stop'][0]['stationinfo'][
+	    '@id']  # Name of the departure station
+	stop_dep = int(
+	    stations_df[stations_df['URI'] == uri_dep].iloc[0, :]['avg_stop_times']
+	)
 	station_dep = uri_dep.split('/')[-1]
 	uri_arr = resp['stops']['stop'][-1]['stationinfo']['@id']
-	stop_arr = int(stations_df[stations_df['URI'] == uri_arr].iloc[0, :]['avg_stop_times'])
+	stop_arr = int(
+	    stations_df[stations_df['URI'] == uri_arr].iloc[0, :]['avg_stop_times']
+	)
 	station_arr = uri_arr.split('/')[-1]
 	line = f'{uri_dep}_{uri_arr}'
 
@@ -85,30 +87,50 @@ def get_features(vehicle_id, time_str):
 			station_cur_uri = station_to_uri(stop['station'])
 			station_cur = station_cur_uri.split('/')[-1]
 			date = datetime.utcfromtimestamp(int(stop['time']))
-			departure_time  = datetime.utcfromtimestamp(int(stop['scheduledDepartureTime']))
-			arrival_time  = datetime.utcfromtimestamp(int(stop['scheduledArrivalTime']))
+			departure_time = datetime.utcfromtimestamp(
+			    int(stop['scheduledDepartureTime'])
+			)
+			arrival_time = datetime.utcfromtimestamp(
+			    int(stop['scheduledArrivalTime'])
+			)
 			dotw = date.weekday()
 			weekend = dotw > 4
 			month = date.month
 			seconds_since_midnight = get_seconds_since_midnight(date)
-			expected_time_station = (departure_time - arrival_time).total_seconds()
+			expected_time_station = (departure_time -
+			                         arrival_time).total_seconds()
 
-			station_info = stations_df[stations_df['URI'] == station_cur_uri].iloc[0, :]
+			station_info = stations_df[stations_df['URI'] == station_cur_uri
+			                          ].iloc[0, :]
 			station_lng = station_info['longitude']
 			station_lat = station_info['latitude']
 			station_stop = station_info['avg_stop_times']
 
-			stuff = [dotw, station_cur, line_id, train_type, station_arr, station_dep, line]
-			encoded_features = ['dotw', 'station_cur', 'line_id', 'train_type', 'station_arr', 'station_dep', 'line']
+			stuff = [
+			    dotw, station_cur, line_id, train_type, station_arr,
+			    station_dep, line
+			]
+			encoded_features = [
+			    'dotw', 'station_cur', 'line_id', 'train_type', 'station_arr',
+			    'station_dep', 'line'
+			]
 			encoded = {}
 			for value, feature in zip(stuff, encoded_features):
 				encoded[feature] = encoders[feature].transform([value])
 
-			vector = [encoded['station_cur'], encoded['station_dep'], encoded['station_arr'], encoded['dotw'], weekend, month, seconds_since_midnight, expected_time_station, station_lng, station_lat, station_stop, encoded['train_type'], encoded['line_id'],stop_arr, stop_dep, encoded['line']]
+			vector = [
+			    encoded['station_cur'], encoded['station_dep'],
+			    encoded['station_arr'], encoded['dotw'], weekend, month,
+			    seconds_since_midnight, expected_time_station, station_lng,
+			    station_lat, station_stop, encoded['train_type'],
+			    encoded['line_id'], stop_arr, stop_dep, encoded['line']
+			]
 
 			before = time.time()
 			delay = cat.predict([vector])[0]
-			delay = np.exp(delay) - 2 + y_min  # NOTE: Model is trained on logarithm!!
+			delay = np.exp(
+			    delay
+			) - 2 + y_min  # NOTE: Model is trained on logarithm!!
 			after = time.time()
 			# print(f'Prediction: {after-before} sec.')
 
@@ -141,4 +163,4 @@ print(get_features('BE.NMBS.IC1517', '11-07-2019 15:17'))
 print(get_features('BE.NMBS.IC1517', '11-07-2019 15:18'))
 print(get_features('BE.NMBS.IC1517', '11-07-2019 15:19'))
 end = time.time()
-print(end-start)
+print(end - start)
