a5550acc37b2210bb38d54fd59a7a802ce5e2d04
Dylan Van Assche <dylan.van.assche@protonmail.com>
2019-07-22 13:05:11 +0200

    Redirect to our Github repo

--- root.py
+++ root.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 from tornado.web import RequestHandler
+GITHUB_REPO = 'https://github.com/oSoc19/reliability-score-backend'
 
 class RootHandler(RequestHandler):
     def set_default_headers(self):
         self.set_header('Access-Control-Allow-Origin', '*')
 
     def get(self):
-        self.write({'message': 'hello world'})
+        self.redirect(GITHUB_REPO, permanent=True)
 
